4e39d840b539344b3055da05ae1b0430e53f712b
Diego Loiola <diego.alves_1996@hotmail.com>
2022-02-10 10:45:00 -0300

    Adicionando a movimentação do fundo do jogo

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,4 +7,16 @@ function start() {
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
+    var jogo = {}
+
+    jogo.timer = setInterval(loop, 30);
+
+    function loop() {
+        movefundo();
+    }
+
+    function movefundo() {
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position", esquerda - 2);
+    }
 }

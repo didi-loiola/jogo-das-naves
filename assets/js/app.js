9bc405198d44e48b429f3cf914ddf18f43068730
Diego Loiola <diego.alves_1996@hotmail.com>
2022-02-15 14:18:49 -0300

    Adicionado Efeito de Explosão

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,10 @@
 function start() {
+    $("#inicio").hide()
 
-    $("#inicio").hide();
-
-    $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
-    $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
-    $("#fundoGame").append("<div id='inimigo2'></div>");
-    $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+    $("#fundoGame").append("<div id='jogador' class='anima1'></div>")
+    $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>")
+    $("#fundoGame").append("<div id='inimigo2'></div>")
+    $("#fundoGame").append("<div id='amigo' class='anima3'></div>")
 
     var jogo = {}
     var TECLA = {
@@ -13,22 +12,22 @@ function start() {
         S: 83,
         D: 68
     }
-    var velocidade = 5;
-    var posicaoY = parseInt(Math.random() * 334);
-    var podeAtirar = true;
+    var velocidade = 5
+    var posicaoY = parseInt(Math.random() * 334)
+    var podeAtirar = true
 
     jogo.pressionou = []
 
     $(document).keydown(function(e) {
-        jogo.pressionou[e.which] = true;
-    });
+        jogo.pressionou[e.which] = true
+    })
 
 
     $(document).keyup(function(e) {
-        jogo.pressionou[e.which] = false;
-    });
+        jogo.pressionou[e.which] = false
+    })
 
-    jogo.timer = setInterval(loop, 30);
+    jogo.timer = setInterval(loop, 30)
 
     function loop() {
         moveFundo()
@@ -40,24 +39,24 @@ function start() {
     }
 
     function moveFundo() {
-        esquerda = parseInt($("#fundoGame").css("background-position"));
-        $("#fundoGame").css("background-position", esquerda - 2);
+        esquerda = parseInt($("#fundoGame").css("background-position"))
+        $("#fundoGame").css("background-position", esquerda - 2)
     }
 
     function moveJogador() {
         if (jogo.pressionou[TECLA.W]) {
-            var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top", topo - 10);
+            var topo = parseInt($("#jogador").css("top"))
+            $("#jogador").css("top", topo - 10)
             if (topo <= 0) {
                 $("#jogador").css("top", topo + 10)
             }
         }
 
         if (jogo.pressionou[TECLA.S]) {
-            var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top", topo + 10);
+            var topo = parseInt($("#jogador").css("top"))
+            $("#jogador").css("top", topo + 10)
             if (topo >= 430) {
-                $("#jogador").css("top", topo - 10);
+                $("#jogador").css("top", topo - 10)
             }
         }
 
@@ -67,14 +66,14 @@ function start() {
     }
 
     function moveInimigo1() {
-        posicaoX = parseInt($("#inimigo1").css("left"));
-        $("#inimigo1").css("left", posicaoX - velocidade);
-        $("#inimigo1").css("top", posicaoY);
+        posicaoX = parseInt($("#inimigo1").css("left"))
+        $("#inimigo1").css("left", posicaoX - velocidade)
+        $("#inimigo1").css("top", posicaoY)
 
         if (posicaoX <= 0) {
-            posicaoY = parseInt(Math.random() * 334);
-            $("#inimigo1").css("left", 694);
-            $("#inimigo1").css("top", posicaoY);
+            posicaoY = parseInt(Math.random() * 334)
+            $("#inimigo1").css("left", 694)
+            $("#inimigo1").css("top", posicaoY)
         }
     }
 
@@ -121,8 +120,31 @@ function start() {
     }
 
     function colisao() {
-        var colisao1 = ($("#jogador").collision($("#inimigo1")));
+        var colisao1 = ($("#jogador").collision($("#inimigo1")))
+        if (colisao1.length > 0) {
+            inimigo1X = parseInt($("#inimigo1").css("left"))
+            inimigo1Y = parseInt($("#inimigo1").css("top"))
+            explosao1(inimigo1X, inimigo1Y)
 
-        console.log(colisao1);
+            posicaoY = parseInt(Math.random() * 334)
+            $("#inimigo1").css("left", 694)
+            $("#inimigo1").css("top", posicaoY)
+        }
+    }
+
+    function explosao1(inimigo1X, inimigo1Y) {
+        $("#fundoGame").append("<div id='explosao1'></div")
+        $("#explosao1").css("background-image", "url(assets/imgs/explosao.png)")
+        var div = $("#explosao1")
+        div.css("top", inimigo1Y)
+        div.css("left", inimigo1X)
+        div.animate({ width: 200, opacity: 0 }, "slow")
+
+        var tempoExplosao = window.setInterval(removeExplosao, 1000)
+
+        function removeExplosao() {
+            div.remove()
+            window.clearInterval(tempoExplosao)
+        }
     }
 }

d4c023c9585e34ead2aa28633256b8a7acaf2b7c
Diego Loiola <diego.alves_1996@hotmail.com>
2022-02-16 11:04:15 -0300

    Inserindo a função de reinicar jogo

--- assets/js/app.js
+++ assets/js/app.js
@@ -323,4 +323,11 @@ function start() {
         $("#fundoGame").append("<div id='fim'></div>")
         $("#fim").html("<h1> Game Over </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>")
     }
+
+}
+
+function reiniciaJogo() {
+    somGameover.pause();
+    $("#fim").remove();
+    start();
 }

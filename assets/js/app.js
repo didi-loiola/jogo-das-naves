31f3741ca0a87860671d61e9b7400d107ea62e1d
Diego Loiola <diego.alves_1996@hotmail.com>
2022-02-16 10:48:01 -0300

    Ajustando a dificuldade conforme abatimento do inimigo1

--- assets/js/app.js
+++ assets/js/app.js
@@ -160,6 +160,7 @@ function start() {
 
         if (colisao3.length > 0) {
             pontos = pontos + 100
+            velocidade += 0.3;
             inimigo1X = parseInt($("#inimigo1").css("left"));
             inimigo1Y = parseInt($("#inimigo1").css("top"));
 
